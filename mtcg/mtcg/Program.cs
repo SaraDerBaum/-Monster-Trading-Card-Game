f52da18919deec3c9d3c3a1fdf236a07190a54bc
Sara Bellus <if19b024@technikum-wien.at>
2020-10-11 19:52:42 +0200

    create project

---
+++ mtcg/mtcg/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mtcg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
